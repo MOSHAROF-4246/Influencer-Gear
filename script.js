a80b1383dd7740bedbbc68523328e3f4e49a87cf
MOSHAROF_HOSSAIN <m.mosharof2015@gmail.com>
2025-07-19 17:28:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toggleBtn = document.getElementById('toggleBtn');
+const navLinks = document.getElementById('navLinks');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
